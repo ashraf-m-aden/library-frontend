b39a9c60b33ba8b9d48c7b88a8e4c44d2a62cd01
nejishow <ashrafmuhamad2014@gmail.com>
2019-11-23 20:53:29 +0300

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,24 @@
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { AddUserComponent } from './users/add-user/add-user.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SidebarComponent } from './navigation/sidebar/sidebar.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', redirectTo: '/dashboard', pathMatch: 'full'
+  }, {
+    path: 'dashboard', component: DashboardComponent, pathMatch: 'full'
+  },
+  {
+    path: 'addUser', component: AddUserComponent, pathMatch: 'full'
+  }
+];
 
+export const _components = [
+  AddUserComponent, SidebarComponent, DashboardComponent
+]
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
+import { AngularMaterialModule } from './angular-material.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, _components } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    _components
+    ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    AngularMaterialModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
