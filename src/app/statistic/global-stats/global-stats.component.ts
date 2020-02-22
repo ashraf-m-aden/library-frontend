4861af478b8703cbe70c7fdfcf110cddb72abd1f
Ashraf Mohamed <41853901+nejishow@users.noreply.github.com>
2020-02-22 07:49:52 +0300

    review

--- src/app/statistic/global-stats/global-stats.component.ts
+++ src/app/statistic/global-stats/global-stats.component.ts
@@ -24,6 +24,19 @@ export class GlobalStatsComponent {
     { data: [65], label: 'Magasine' },
     { data: [28], label: 'Bande dessinée' }
   ];
+
+  displayedColumns: string[] = ['Type', 'totalNumber', 'absentNumber'];
+  ELEMENT_DATA = [
+    { Type: 'Roman', totalNumber: 1.0079, absentNumber: 'H' },
+    { Type: 'Journale', totalNumber: 4.0026, absentNumber: 'He' },
+    { Type: 'Conte', totalNumber: 6.941, absentNumber: 'Li' },
+    { Type: 'Bande dessinée', totalNumber: 9.0122, absentNumber: 'Be' },
+    { Type: 'Biographie', totalNumber: 10.811, absentNumber: 'B' },
+    { Type: 'Magasine', totalNumber: 12.0107, absentNumber: 'C' },
+
+  ];
+  dataSource = this.ELEMENT_DATA; // nescessaire
+
   constructor() {
     //
   }
